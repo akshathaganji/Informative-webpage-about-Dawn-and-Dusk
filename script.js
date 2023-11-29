e64107dab223bc87279990b091a2966db22a5927
akshatha-ganji <aganji1@hawk.iit.edu>
2023-11-28 21:43:06 -0600

    new

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
+function fetchlocation() {
+    navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        const url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                document.querySelector('#timezone2').textContent = data.results.timezone;
+                displayData(data, '');
+                fetchTomorrowData(latitude, longitude);
+            })
+            .catch(error => alert('No access by your browser'));
+    });
+}
+
 function showtime() {
     const location = document.getElementById('locationInput').value;
-    const geocodeUrl = `https://geocode.maps.co/search?q=${location}`;
-    fetch(geocodeUrl)
-      .then(response => response.json())
-      .then(data => {
-        const latitude = data[0].lat;
-        const longitude = data[0].lon;
-  
-        const todayUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=today`;
-        const tomorrow = new Date();
-        tomorrow.setDate(tomorrow.getDate() + 1);
-        const tomorrowUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=${tomorrow.toISOString().split('T')[0]}`;
-  
-        fetch(todayUrl)
-          .then(response => response.json())
-          .then(data => {
-            displayData(data, '');
-          })
-          .catch(error => {
-            displayError('Error fetching data for today:', error);
-          });
-  
-        fetch(tomorrowUrl)
-          .then(response => response.json())
-          .then(data => {
-            displayData(data, '1');
-          })
-          .catch(error => {
-            displayError('Error fetching data for tomorrow:', error);
-          });
-      })
-      .catch(error => {
-        displayError('Error with geocoding API:', error);
-      });
-  }
-  function displayData(data, suffix) {
+    if (location.trim() === '') {
+        alert('Please enter a valid location');
+        return;
+    }
+
+    const loc = `https://geocode.maps.co/search?q=${location}`;
+    fetch(loc)
+        .then(response => response.json())
+        .then(data => {
+            const latitude = data[0].lat;
+            const longitude = data[0].lon;
+            const place = data[0].display_name;
+            document.getElementById('place').textContent = "More closest location you searched for =  " + place;
+
+            fetchSunriseSunsetData(latitude, longitude);
+            fetchTomorrowData(latitude, longitude);
+        })
+        .catch(error => alert('Not a valid location'));
+}
+
+function displayData(data, suffix) {
     document.querySelector(`#sunrise${suffix}`).textContent = data.results.sunrise;
     document.querySelector(`#sunset${suffix}`).textContent = data.results.sunset;
     document.querySelector(`#dawn${suffix}`).textContent = data.results.dawn;
@@ -42,17 +43,46 @@ function showtime() {
     document.querySelector(`#day_length${suffix}`).textContent = data.results.day_length;
     document.querySelector(`#solar_noon${suffix}`).textContent = data.results.solar_noon;
     document.querySelector(`#timezone${suffix}`).textContent = data.results.timezone;
-    document.querySelector(`#raw-output${suffix}`).textContent = JSON.stringify(data);
-  
-    // Hide error message if data is displayed successfully
-    document.getElementById('error-message').textContent = '';
-  }
-  function displayError(message, error) {
-    const errorMessage = `${message} ${error}`;
-    console.error(errorMessage);
-  document.getElementById('error-message').textContent = errorMessage;
-  document.querySelectorAll('.data span').forEach(span => {
-      span.textContent = '';
-    });
-  }
-  
+}
+
+function fetchSunriseSunsetData(latitude, longitude) {
+    const todayUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=today`;
+
+    fetch(todayUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayData(data, '');
+        })
+        .catch(error => {
+            displayError('Error fetching data for today:', error);
+        });
+}
+
+function getTomorrowDate() {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    return tomorrow.toISOString().split('T')[0];
+}
+
+function fetchTomorrowData(latitude, longitude) {
+    const tomorrowUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=${getTomorrowDate()}`;
+
+    fetch(tomorrowUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to fetch data for tomorrow: ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayData(data, '1');
+        })
+        .catch(error => {
+            displayError('Error fetching data for tomorrow:', error);
+        });
+}
